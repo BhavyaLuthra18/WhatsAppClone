efb469d8af2a59e46def10268a1f4f1297b4543f
Bhavya Luthra <bhavyaluthra1234@gmail.com>
2024-10-18 14:13:01 +0530

    addition

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [""],
+    origin: ["whats-app-clone-fv7y.vercel.app"],
     optionsSuccessStatus: 200,
     methods: ["POST", "GET"],
     credentials: true,
